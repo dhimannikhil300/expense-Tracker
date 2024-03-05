fa12f79a95e5753f7a3d3c54991f94c4b4134d29
Nikhil Dhiman <dhimannikhil300@gmail.com>
2024-03-06 00:05:51 +0530

    Change in Context.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "https://studynotion-backend-dlbe.onrender.com/api/v1";
+const BASE_URL = "https://studynotion-backend-dlbe.onrender.com/api/v1/";
 
 
 const GlobalContext = React.createContext()
